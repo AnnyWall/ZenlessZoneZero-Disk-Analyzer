05cc33436a1ad7532ba534410ea219741632f2fa
AnnyWall <minep2223@gmail.com>
2025-11-19 15:55:18 +0700

    Second Version

--- zzz_profiler/run.py
+++ zzz_profiler/run.py
@@ -9,7 +9,8 @@ def run_server():
     """Функция для запуска сервера, которую мы будем вызывать из __main__.py."""
     # use_reloader=False ОБЯЗАТЕЛЕН для стабильного запуска в потоке.
     # debug=False рекомендуется для фонового режима, чтобы избежать лишних логов.
-    app.run(port=5000, use_reloader=False, debug=False)
+    # threaded=True для обработки нескольких запросов одновременно
+    app.run(port=5000, use_reloader=False, debug=False, threaded=True)
 
 if __name__ == '__main__':
     # Эта часть нужна, только если вы хотите запустить сервер отдельно для теста.
